e5be7a681110624075ba0bd0b5ba5218a61d6e87
Léo Andéol <leo.andeol@gmail.com>
2018-05-23 17:41:40 +0200

    Updating gitignore and created two classes for future AIs

---
+++ ai/sd.py
@@ -0,0 +1,35 @@
+from ai import AI
+import random
+import collections
+
+class SDAI(AI):
+    """
+    Smart drafting : plays randomly like the stupid AI, except for the drafting (placement) phase where it uses MCTS with UCT to find the best countries.
+    """
+
+    def start(self):
+        #0 if first, 1 if second etc
+        self.play_order=self.game.turn_order.index(self.player.name)
+        
+    
+    def initial_placement(self, empty, remaining):
+        if empty:
+            return random.choice(empty)
+        else:
+            t = random.choice(list(self.player.territories))
+            return t
+
+    def attack(self):
+        for t in self.player.territories:
+            for a in t.connect:
+                if a.owner != self.player:
+                    if t.forces > a.forces:
+                        yield (t, a, None, None)
+
+    def reinforce(self, available):
+        border = [t for t in self.player.territories if t.border]
+        result = collections.defaultdict(int)
+        for i in range(available):
+            t = random.choice(border)
+            result[t] += 1
+        return result
